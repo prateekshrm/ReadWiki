0b00714ad9ab0b881dea479f50a1d703a8a82fe8
Prateek Sharma <11prateek12@gmail.com>
2026-08-27 12:05:28 +0530

    fix: fixed Loader component shifting abruptly

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,21 +1,40 @@
 import Colors from "@/constants/Colors";
 import { ContainedLoadingIndicator, Host } from "@expo/ui/jetpack-compose";
-import { ActivityIndicator, Platform, StyleSheet } from "react-native";
+import { ActivityIndicator, Platform, StyleSheet, View } from "react-native";
 
 const Loader = () => {
     if (Platform.OS === "ios") {
-        return <ActivityIndicator size="large" />;
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color={Colors.primary} />
+            </View>
+        );
     }
     return (
-        <Host matchContents>
-            <ContainedLoadingIndicator
-                containerColor={Colors.primary}
-                color={Colors.background}
-            />
-        </Host>
+        <View style={styles.container}>
+            <Host matchContents style={styles.host}>
+                <ContainedLoadingIndicator
+                    containerColor={Colors.primary}
+                    color={Colors.background}
+                />
+            </Host>
+        </View>
     );
 };
 
 export default Loader;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        width: 48,
+        height: 48,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    host: {
+        width: 48,
+        height: 48,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
