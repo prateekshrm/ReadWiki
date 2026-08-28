ab0589acf02257dfe9fac6fc23b5a64f0d40828d
Prateek Sharma <11prateek12@gmail.com>
2026-08-28 13:40:20 +0530

    chore: update app version to 1.2.0 in configuration

--- app.config.ts
+++ app.config.ts
@@ -14,7 +14,7 @@ export default (): ExpoConfig => {
               : "ReadWiki",
 
         slug: "ReadWiki",
-        version: "1.1.0",
+        version: "1.2.0",
         orientation: "portrait",
 
         icon: "./assets/images/icon.png",
